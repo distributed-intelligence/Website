27a12e43a9e2ebbd437cfc76a4e4ea149106dfd1
distributed-intelligence <33647132+distributed-intelligence@users.noreply.github.com>
2017-11-17 17:18:03 -0500

    inline CSS; minified html

--- critical.js
+++
@@ -1,9 +0,0 @@
-var critical = require('critical');
-
-critical.generate({
-    inline: true,
-    base: 'working_files/',
-    src: 'index_unoptimized.html',
-    dest: '../public/index-critical.html',
-    minify: true,
-});
